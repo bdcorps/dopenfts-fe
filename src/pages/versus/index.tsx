00dc548ff93760a95931356d8f257fba71d83527
shaan <hashmishaana@gmail.com>
2022-02-03 22:25:23 -0500

    Added Animations to the Versus Images
    -Utilizing Animate.CSS library
    - Both Images in VS screen come sliding in from the sides
    - Images shake when clicked

--- src/pages/versus/index.tsx
+++ src/pages/versus/index.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import "./../../index.css";
 import Header from "./../header";
 import SubHeader from "./../subheader";
+import "animate.css";
 
 interface versusProps {
   imageOneLink: string;
@@ -9,6 +10,26 @@ interface versusProps {
 }
 
 const Versus: FunctionComponent<versusProps> = (props) => {
+  const [loadAniLeft, setloadAniLeft] = useState(true);
+  const [shakeLeft, setShakeLeft] = useState(false);
+  const [loadAniRight, setloadAniRight] = useState(true);
+  const [shakeRight, setShakeRight] = useState(false);
+
+  const animationEnded = () => {
+    setloadAniLeft(false);
+    setShakeLeft(false);
+    setloadAniRight(false);
+    setShakeRight(false);
+  };
+
+  const mouseClickedLeft = () => {
+    setShakeLeft(true);
+  };
+
+  const mouseClickedRight = () => {
+    setShakeRight(true);
+  };
+
   return (
     <div className="bg-primary">
       <Header />
@@ -17,11 +38,25 @@ const Versus: FunctionComponent<versusProps> = (props) => {
       <div className="d-flex just-cont-center py-2">
         <div className="grid-sym-5">
           <div className="versus-image-container-1">
-            <img src={props.imageOneLink} className="versus-image"></img>
+            <img
+              src={props.imageOneLink}
+              className={`versus-image animate__animated ${
+                loadAniLeft ? "animate__slideInLeft " : ""
+              } ${shakeLeft ? "animate__wobble" : ""}`}
+              onAnimationEnd={animationEnded}
+              onClick={mouseClickedLeft}
+            ></img>
           </div>
           <div className="versus-text fs-1 font-color-secondary">VS!</div>
           <div className="versus-image-container-2">
-            <img src={props.imageTwoLink} className="versus-image"></img>
+            <img
+              src={props.imageTwoLink}
+              className={`versus-image animate__animated ${
+                loadAniRight ? "animate__slideInRight " : ""
+              } ${shakeRight ? "animate__wobble" : ""}`}
+              onAnimationEnd={animationEnded}
+              onClick={mouseClickedRight}
+            ></img>
           </div>
         </div>
       </div>
